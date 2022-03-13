b37bed5d3902de96d91b5f0e466fde9214d6ab2c
Steven <stevennassef97@gmail.com>
2022-03-13 13:47:13 +0200

    Added Project Initializer

---
+++ Assets/_YajuluSDK/_Scripts/ProjectInitializer.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace PROJECT.Scripts
+{
+	[InitializeOnLoad]
+	public class ProjectInitializer {
+		static ProjectInitializer()
+		{
+			Debug.Log("Initializing Project...");
+			UpdateKeyStorePath();
+			Debug.Log("InitializationComplete.");
+		}
+		
+		[Button]
+		private static void UpdateKeyStorePath()
+		{
+			var yajuluPath = Environment.GetEnvironmentVariable("Yajulu", EnvironmentVariableTarget.User);
+			if (yajuluPath == null)
+			{
+				Debug.LogError("Can't find Environment Variable 'Yajulu', Please set it in the User Environment Varaiables.");
+				return;
+			}
+			
+			var vProductName = PlayerSettings.productName;
+			var keyStorePath = $"{yajuluPath}\\{vProductName}\\keystore\\{vProductName}.keystore";
+			if(!File.Exists(keyStorePath))
+			{    
+				Debug.LogError($"Cannot find a keystore with this path {keyStorePath}");
+				
+				return;
+			}
+			PlayerSettings.Android.keystoreName = keyStorePath;
+			Debug.Log($"KeyStore Path Updated to {keyStorePath}");
+		}
+	}
+	
+
+}
