9c91ad4357f01d503876a9ddcbb68996a785034c
Steven <stevennassef97@gmail.com>
2022-03-18 15:14:29 +0200

    Updated Playfab Test

--- Assets/PROJECT/Scripts/PluginsTestScript.cs
+++ Assets/PROJECT/Scripts/PluginsTestScript.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using Facebook.Unity;
 public class PluginsTestScript : MonoBehaviour
 {
-   
-   void Awake ()
+    private void Awake ()
     {
         if (!FB.IsInitialized) {
             // Initialize the Facebook SDK
@@ -17,67 +16,67 @@ public class PluginsTestScript : MonoBehaviour
         
     }
     private void InitCallback ()
-{
-    if (FB.IsInitialized) {
-        // Signal an app activation App Event
-        FB.ActivateApp();
-        // Continue with Facebook SDK
-        // ...
-    } else {
-        Debug.Log("Failed to Initialize the Facebook SDK");
-    }
-}
-
-private void OnHideUnity (bool isGameShown)
-{
-    if (!isGameShown) {
-        // Pause the game - we will need to hide
-        Time.timeScale = 0;
-    } else {
-        // Resume the game - we're getting focus again
-        Time.timeScale = 1;
-    }
-}
-
-private void LoginStatusCallback(ILoginStatusResult result) 
-{
-    if (!string.IsNullOrEmpty(result.Error)) {
-        Debug.Log("Error: " + result.Error);
-    } else if (result.Failed) {
-        Debug.Log("Failure: Access Token could not be retrieved");
-    } else {
-        // Successfully logged user in
-        // A popup notification will appear that says "Logged in as <User Name>"
-        Debug.Log("Success: " + result.AccessToken.UserId);
-    }
-}
-
-
-private void AuthCallback (ILoginResult result) {
-    if (FB.IsLoggedIn) {
-        // AccessToken class will have session details
-        var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
-        // Print current access token's User ID
-        Debug.Log( "USER ID" + aToken.UserId);
-        
-        // Print current access token's granted permissions
-        foreach (string perm in aToken.Permissions) {
-            Debug.Log("PARAMETERS " + perm);
+    {
+        if (FB.IsInitialized) {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        } else {
+            Debug.Log("Failed to Initialize the Facebook SDK");
         }
-    } else {
-        Debug.Log("User cancelled login");
     }
-}
+
+    private void OnHideUnity (bool isGameShown)
+    {
+        if (!isGameShown) {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        } else {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+    private void LoginStatusCallback(ILoginStatusResult result) 
+    {
+        if (!string.IsNullOrEmpty(result.Error)) {
+            Debug.Log("Error: " + result.Error);
+        } else if (result.Failed) {
+            Debug.Log("Failure: Access Token could not be retrieved");
+        } else {
+            // Successfully logged user in
+            // A popup notification will appear that says "Logged in as <User Name>"
+            Debug.Log("Success: " + result.AccessToken.UserId);
+        }
+    }
 
 
-public void FBExpressLoginTest()
-{
-    //FB.Android.RetrieveLoginStatus(LoginStatusCallback);
-    var perms = new List<string>(){"public_profile", "email"};
-    FB.LogInWithReadPermissions(perms, AuthCallback);
+    private void AuthCallback (ILoginResult result) {
+        if (FB.IsLoggedIn) {
+            // AccessToken class will have session details
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log( "USER ID" + aToken.UserId);
+        
+            // Print current access token's granted permissions
+            foreach (string perm in aToken.Permissions) {
+                Debug.Log("PARAMETERS " + perm);
+            }
+        } else {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+
+    public void FBExpressLoginTest()
+    {
+        //FB.Android.RetrieveLoginStatus(LoginStatusCallback);
+        var perms = new List<string>(){"public_profile", "email"};
+        FB.LogInWithReadPermissions(perms, AuthCallback);
    
 
 
    
-}
+    }
 }
--- Assets/_YajuluSDK/_Scripts/Social/PlayfabFacebookAuthExample.cs
+++ Assets/_YajuluSDK/_Scripts/Social/PlayfabFacebookAuthExample.cs
@@ -21,21 +21,37 @@ namespace _YajuluSDK._Scripts.Social
             SetMessage("Initializing Facebook..."); // logs the given message and displays it on the screen using OnGUI method
 
             // This call is required before any other calls to the Facebook API. We pass in the callback to be invoked once initialization is finished
-            FB.Init(OnFacebookInitialized);  
+            FB.Init(OnFacebookInitialized, OnHideUnity);  
 
         }
 
         private void OnFacebookInitialized()
         {
-            SetMessage("Logging into Facebook...");
-
+            
             // Once Facebook SDK is initialized, if we are logged in, we log out to demonstrate the entire authentication cycle.
             if (FB.IsLoggedIn)
                 FB.LogOut();
-
+            SetMessage("Facebook Initialized.");
+        }
+        
+        public void LoginWithFacebook()
+        {
+            SetMessage("Logging into Facebook...");
             // We invoke basic login procedure and pass in the callback to process the result
             FB.LogInWithReadPermissions(new List<string>() {"public_profile", "email", "first_name"}, OnFacebookLoggedIn);
         }
+        
+        private void OnHideUnity (bool isGameShown)
+        {
+            if (!isGameShown) {
+                // Pause the game - we will need to hide
+                Time.timeScale = 0;
+            } else {
+                // Resume the game - we're getting focus again
+                Time.timeScale = 1;
+            }
+        }
+
 
         private void OnFacebookLoggedIn(ILoginResult result)
         {
@@ -44,11 +60,11 @@ namespace _YajuluSDK._Scripts.Social
             {
                 SetMessage($"Facebook Auth Complete! Access Token: " + AccessToken.CurrentAccessToken.TokenString + $"\nLogging into PlayFab...  {AccessToken.CurrentAccessToken.UserId}");
 
-                foreach (var per in AccessToken.CurrentAccessToken.Permissions)
-                {
-                    Debug.Log(per);
-                }
-                
+                // foreach (var per in AccessToken.CurrentAccessToken.Permissions)
+                // {
+                //     Debug.Log(per);
+                // }
+                //
                 /*
              * We proceed with making a call to PlayFab API. We pass in current Facebook AccessToken and let it create
              * and account using CreateAccount flag set to true. We also pass the callback for Success and Failure results
