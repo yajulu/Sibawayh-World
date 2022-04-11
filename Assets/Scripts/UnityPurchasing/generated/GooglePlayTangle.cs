b9c71da61d0e2d0a128320023efa6fe3b506b6e6
Omar <omarihabelsayed@gmail.com>
2022-04-11 15:26:56 +0200

    Fixing Dependency Manager Conflicts

--- Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
+++ Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
@@ -3,9 +3,9 @@
 namespace UnityEngine.Purchasing.Security {
     public class GooglePlayTangle
     {
-        private static byte[] data = System.Convert.FromBase64String("VdrtG4mWmyPnJ3PG2r1LhRH43dTgEi30GmXy0mMPaoRs2cJJBAf0OhKRn5CgEpGakhKRkZAQew7ti9OXeRVeIlue4mWoTR/3WQeCAkwah+UTx2G+ycm5AeCyXzvEM6r+2sKE26ASkbKgnZaZuhbYFmedkZGRlZCTwugQ289bStQStpqhZADzYXXhDedUsR+jtdQfBpPLKQwV2QdfxEr8OllhTz0WZthfytuFRjL8lvyeqwp6EmEsB9RwRXiTk+82wXkntpmWFzLLI+LALr5yrvOPNCT3SXvzJcLpRM8CptCcXWYdNe1cccu9802H1O4I4pwrOHCaPfMkvnnnhAOc87xlfMoz+CTw9kSInOGhc1PDz7bYLxaFJOYt6+yJmGP9xZKTkZCR");
-        private static int[] order = new int[] { 13,6,6,7,6,13,9,8,10,10,10,13,13,13,14 };
-        private static int key = 144;
+        private static byte[] data = System.Convert.FromBase64String("uAqJqriFjoGiDsAOf4WJiYmNiIvTO/rYNqZqtuuXLDzvUWPrPdrxXAvfeabR0aEZ+KpHI9wrsubC2pzD+Ao17AJ96sp7F3KcdMHaURwf7CJMqQe7rcwHHovTMRQNwR9H3FLkItcavsiERX4FLfVEadOl61WfzPYQComHiLgKiYKKComJiAhjFvWTy4/a8AjD10NSzAqugrl8GOt5bfkV//qEMyBogiXrPKZh/5wbhOukfWTSCnk0H8xoXWCLi/cu2WE/roGODypNwvUDkY6DO/8/a97CpVOdCeDFzCvgPOjuXJCE+blrS9vXrsA3Dp08QXlXJQ5+wEfSw51eKuSO5IazEmJhDUY6Q4b6fbBVB+9BH5oaVAKf/f418/SRgHvl3YqLiYiJ");
+        private static int[] order = new int[] { 0,4,2,6,8,5,8,9,11,11,13,12,13,13,14 };
+        private static int key = 136;
 
         public static readonly bool IsPopulated = true;
 
