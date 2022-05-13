f4fc9490cd5fd3af8b075119a85ecdcd18ce8654
Omar <omarihabelsayed@gmail.com>
2022-05-14 01:00:02 +0200

    Adding Animations to Home Screen

--- Assets/PROJECT/Scripts/UI/Screens/Screen_HomeScreen.cs
+++ Assets/PROJECT/Scripts/UI/Screens/Screen_HomeScreen.cs
@@ -1,10 +1,25 @@
 using _YajuluSDK._Scripts.UI;
+using DG.Tweening;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PROJECT.Scripts.UI.Screens
 {
     public class Screen_HomeScreen : UIScreenBase
     {
-        // Start is called before the first frame update
+        [SerializeField]
+        Transform logo_Image, buttons_Panel;
+
+        private float buttons_panelEndValue;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            buttons_panelEndValue = buttons_Panel.localPosition.x;
+
+        }
         void Start()
         {
 
@@ -14,6 +29,42 @@ namespace PROJECT.Scripts.UI.Screens
         void Update()
         {
 
+        }
+
+
+
+        protected override void OnSkipOpenAnimation()
+        {
+            logo_Image.transform.DOComplete();
+        }
+
+        protected override void OnSkipCloseAnimation()
+        {
+            logo_Image.transform.DOComplete();
+        }
+
+
+
+        protected override void OpenAnimation()
+        {
+            gameObject.SetActive(true);
+            logo_Image.DOScale(1, 0.6f)
+                .SetEase(Ease.OutBounce)
+                .From(0)
+                .OnComplete(base.OpenAnimation);
+
+            buttons_Panel.DOLocalMoveX(buttons_panelEndValue, 0.6f)
+                .SetEase(Ease.OutBounce)
+                .From(buttons_panelEndValue - 200)
+                .OnComplete(base.OpenAnimation);
+        }
+
+        protected override void CloseAnimation()
+        {
+            logo_Image.transform.DOScale(0, 0.3f)
+                .SetEase(Ease.InBack)
+                .From(1)
+                .OnComplete(base.CloseAnimation);
         }
     }
 }
