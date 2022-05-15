29864123a9a3425b891130899f9175106d8c61db
Steven <stevennassef97@gmail.com>
2022-05-16 01:48:16 +0200

    Build Fixes

--- Assets/_YajuluSDK/_Scripts/Essentials/RTLStringAttributeProcessor.cs
+++ Assets/_YajuluSDK/_Scripts/Essentials/RTLStringAttributeProcessor.cs
@@ -1,7 +1,9 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using RTLTMPro;
 using Sirenix.OdinInspector;
+
 using Sirenix.OdinInspector.Editor;
 
 namespace _YajuluSDK._Scripts.Essentials
@@ -9,6 +11,7 @@ namespace _YajuluSDK._Scripts.Essentials
 	public class RTLStringAttributeProcessor : OdinAttributeProcessor<string>
 	{
 		private string fixedText;
+		private static FastStringBuilder finalText = new FastStringBuilder(200); 
 		public override void ProcessSelfAttributes(InspectorProperty property, List<Attribute> attributes)
 		{
 			// attributes.Add(new OnValueChangedAttribute("@RTLStringAttributeProcessor.GetFixedText($value)"));
@@ -19,7 +22,6 @@ namespace _YajuluSDK._Scripts.Essentials
 		{
 			if (string.IsNullOrEmpty(input))
 				return input;
-			FastStringBuilder finalText = new FastStringBuilder(input.Length); 
 			finalText.Clear();
 			RTLSupport.FixRTL(input, finalText, false, false, false);
 			// finalText.Reverse();
@@ -29,4 +31,4 @@ namespace _YajuluSDK._Scripts.Essentials
 		
 	}
 }
-
+#endif
