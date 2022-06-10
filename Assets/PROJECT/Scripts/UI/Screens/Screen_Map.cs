02072b23b528954d4adfb4a6ea6baa50c033258f
Steven <stevennassef97@gmail.com>
2022-06-10 02:10:18 +0200

    Basic Map Layout

---
+++ Assets/PROJECT/Scripts/UI/Screens/Screen_Map.cs
@@ -0,0 +1,20 @@
+using _YajuluSDK._Scripts.UI;
+using UnityEngine.UI;
+
+namespace PROJECT.Scripts.UI.Screens
+{
+    public class Screen_Map : UIScreenBase
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
